45d38ab60d1efd37a8fa4269c5a05530d667a4db
Michael Vaughn <MikishVaughn@users.noreply.github.com>
2025-08-06 18:41:29 -0500

    Remove

--- SIBulldogs/Controllers/InfoController.cs
+++ SIBulldogs/Controllers/InfoController.cs
@@ -19,11 +19,7 @@ public class InfoController : Controller   {
         ViewBag.Message = "About";            
         return View("Index");
     }
-    public IActionResult Contact()
-    {
-        ViewBag.Message = "Contact";                       
-        return View();
-    }
+   
     public IActionResult TOS()
     {
         ViewBag.Message = "TOS";
@@ -34,52 +30,7 @@ public class InfoController : Controller   {
         ViewBag.Message = "Privacy";
         return View();
     }
-    public IActionResult Calendar()        
-    {
-        ViewBag.Message = "Calendar";            
-        return View();
-    }
-     public IActionResult Fundraise()
-    {
-        ViewBag.Message = "Fundraise";
-        return View();
-    }
-    public IActionResult Bilaws()
-    {
-        ViewBag.Message = "Bilaws";
-        return View();
-    }
-    public IActionResult ClubInfo()
-    {
-        ViewBag.Message = "ClubInfo";
-        return View();
-    }
-     public IActionResult Minutes()
-    {
-        ViewBag.Message = "Minutes";
-        return View();
-    }
-     public IActionResult Flyer()
-    {
-        ViewBag.Message = "Flyer";
-        return View();
-    }
-    public IActionResult TrackWrestling()
-    {
-        ViewBag.Message = "TrackWrestling";
-        return View();
-    }
-    //public IActionResult ClubBanner()
-    //{
-    //    ViewBag.Message = "ClubBanner";
-    //    return View();
-    //}
-
-    public IActionResult Test()
-    {
-        ViewBag.Message = "Test";
-        return View();
-    }
+   
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
